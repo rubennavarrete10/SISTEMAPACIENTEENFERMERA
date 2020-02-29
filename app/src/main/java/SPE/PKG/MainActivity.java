49208f5df787568a4d7cbbe4eebbe92119cdd487
owner <rubendiaznt@live.com.mx>
2020-02-28 18:32:42 -0600

    Initial commit

---
+++ app/src/main/java/SPE/PKG/MainActivity.java
@@ -0,0 +1,107 @@
+package SPE.PKG;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ScheduledExecutorService;
+import android.content.Context;
+import android.os.Vibrator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Handler.Callback;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView fecha;
+    TextView hora;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button ASISTENCIA = (Button) findViewById(R.id.ASISTENCIA);
+        Button EMERGENCIA = (Button) findViewById(R.id.EMERGENCIA);
+
+        fecha = (TextView) findViewById(R.id.fecha);
+        hora = (TextView) findViewById(R.id.hora);
+
+        EMERGENCIA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                Date date = new Date();
+                String fechafinal = dateFormat.format(date);
+                fecha.setText(fechafinal);
+                WEBSERVICE("http://direccion de correo");
+            }
+        });
+
+        ASISTENCIA.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm:ss");
+                Date date = new Date();
+                String horafinal = dateFormat.format(date);
+                hora.setText(horafinal);
+            }
+        });
+    }
+    private void WEBSERVICE (String URL){
+        StringRequest request= new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), "OPERACION EXITOSA", Toast.LENGTH_SHORT).show();
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();;
+            }
+        }) {
+            @Override
+            protected Map <String, String> getParams() throws AuthFailureError{
+                Map<String, String> parametros= new HashMap<String, String>();
+                parametros.put("codigo",fecha.getText().toString());
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(request);
+    }
+}
