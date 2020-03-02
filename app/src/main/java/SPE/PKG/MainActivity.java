cc00e144f3ce2cd350de8cfe01974e71a9d1486e
owner <rubendiaznt@live.com.mx>
2020-03-02 16:21:09 -0600

    fecha,hora,checa sd, reproduce urls

--- app/src/main/java/SPE/PKG/MainActivity.java
+++ app/src/main/java/SPE/PKG/MainActivity.java
@@ -1,38 +1,29 @@
 package SPE.PKG;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.speech.RecognizerIntent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.io.File;
+import java.io.IOException;
+import android.os.Environment;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.ScheduledExecutorService;
-import android.content.Context;
 import android.os.Vibrator;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.os.Handler.Callback;
-
-
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -41,23 +32,30 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-
 public class MainActivity extends AppCompatActivity {
 
+    private static final int RECOGNIZE_SPEECH_ACTIVITY = 1;
     TextView fecha;
     TextView hora;
     TextView recovoz;
-    private static final int RECOGNIZE_SPEECH_ACTIVITY = 1;
+
+    String localizacion = "/storage/emulated/0/Pictures/Messenger";
+    //String localizacion = "Tarjeta SD/Pictures/Screenshots/file.mp3";
+    //String localizacion = "https://sampleswap.org/samples-ghost/MELODIC%20SAMPLES/GUITARS/146[kb]badmetal2.aif.mp3";///funciona
+    Uri uri = Uri.parse(localizacion);
+
+    MediaRecorder grabacion = null;
+    private MediaPlayer player;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tarjetaSd();
 
         Button ASISTENCIA = (Button) findViewById(R.id.ASISTENCIA);
         Button EMERGENCIA = (Button) findViewById(R.id.EMERGENCIA);
-
         fecha = (TextView) findViewById(R.id.fecha);
         hora = (TextView) findViewById(R.id.hora);
         recovoz= (TextView) findViewById(R.id.timer);
@@ -70,25 +68,29 @@ public class MainActivity extends AppCompatActivity {
                 String fechafinal = dateFormat.format(date);
                 fecha.setText(fechafinal);
                // WEBSERVICE("https://localhost:44370/WebService1.asmx");/////////////////////////////////////////////////////////PONER UN URL NO USAR POR EL MOMENTO
+               // reconocer();
+                reproducir();
+
             }
         });
 
-        ASISTENCIA.setOnClickListener(new View.OnClickListener() {
-            @Override
+        if (tarjetaSd()== true) {
+            ASISTENCIA.setOnClickListener(new View.OnClickListener() {
+                @Override
 
-            public void onClick(View v) {
+                public void onClick(View v) {
 
-                SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm:ss");
-                Date date = new Date();
-                String horafinal = dateFormat.format(date);
-                hora.setText(horafinal);
-                reconocer();
-            }
-        });
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm:ss");
+                    Date date = new Date();
+                    String horafinal = dateFormat.format(date);
+                    hora.setText(horafinal);
+                    grabar();
 
-
-
+                }
+            });
+        }
     }
+/////////////////////////////////////////////////////////////////////////////////// voz to texto //////////////////////////////////////////////
     private void reconocer(){
         new Thread(new Runnable() {
             @Override
@@ -107,9 +109,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }).start();
-
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -117,35 +117,15 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case RECOGNIZE_SPEECH_ACTIVITY:
                 if (resultCode == RESULT_OK && null != data) {
-                    ArrayList<String> speech = data
-                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    ArrayList<String> speech = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     String strSpeech2Text = speech.get(0);
                     recovoz.setText(strSpeech2Text);
-
-
                 }
             default:
                 break;
-
         }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+////////////////////////////////////////////////////////////webservice///////////////////////////////////////////////////////////////////////////////////
     private void WEBSERVICE (String URL){
         StringRequest request= new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
             @Override
@@ -169,4 +149,68 @@ public class MainActivity extends AppCompatActivity {
         RequestQueue requestQueue= Volley.newRequestQueue(this);
         requestQueue.add(request);
     }
+    ///////////////////////////////////////////////////////grabar audio///////////////////////////////////////////
+    public void grabar (){
+        if(grabacion == null){
+
+            grabacion = new MediaRecorder();
+            grabacion.setAudioSource(MediaRecorder.AudioSource.MIC);
+            grabacion.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            grabacion.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+            grabacion.setOutputFile(localizacion);//////////////////////////////////////////////////////////////////error al sacar el archivo de audio
+            Toast.makeText(getApplicationContext(), "empezaste a grabar", Toast.LENGTH_SHORT).show();
+            try {
+                grabacion.prepare();
+                grabacion.start();
+            }
+            catch (IOException e){
+
+            }
+        }else if (grabacion != null){
+            grabacion.stop();
+            grabacion.release();
+            grabacion = null;
+            Toast.makeText(getApplicationContext(), "dejaste de grabar", Toast.LENGTH_SHORT).show();
+        }
+    }
+    ///////////////////////////////////////////////////////////reproducir audio/////////////////////
+    public void reproducir (){
+        if(grabacion == null){
+            player = new MediaPlayer();
+            player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            try {
+                player.setDataSource(getApplicationContext(),uri);
+                player.prepareAsync();
+                player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mediaPlayer) {
+                        Toast.makeText(getApplicationContext(), "Reproduciendo audio", Toast.LENGTH_SHORT).show();
+                        if (player.isPlaying()){
+                            player.pause();
+                        }else{
+                            player.start();
+                        }
+                    }
+                });
+            }
+            catch (IOException e){
+
+            }
+            player = MediaPlayer.create(this, uri);
+            Toast.makeText(getApplicationContext(), "No se puede reproducir", Toast.LENGTH_SHORT).show();
+        }
+    }
+    /////////////////////////////////////////////////////////////revisar sd///////////////
+    public boolean tarjetaSd() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            Toast.makeText(getApplicationContext(), "SD listo", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        Toast.makeText(getApplicationContext(), "Error SD", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+    ///////////////////////////////////////////crear archivos ///////////////////////////////
+
 }
+
